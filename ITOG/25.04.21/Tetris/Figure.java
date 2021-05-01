29fdf40c9469b735dc0302a0b6213921f128f7f6
GarlicRoll <75137969+GarlicRoll@users.noreply.github.com>
2021-05-01 19:57:03 +0300

    Add files via upload
    Files + plans

---
+++ ITOG/25.04.21/Tetris/Figure.java
@@ -0,0 +1,39 @@
+package Tetris;
+
+import java.util.ArrayList;
+
+import org.eclipse.swt.graphics.Image;
+
+public class Figure {
+
+	ArrayList<Image> images;
+	
+	Figure(ArrayList<Image> images){
+		this.images = images; 
+	}
+	
+	void rotate() {
+		
+	}
+	
+	void move_down() {
+		
+	}
+	
+	void move_right() {
+		
+	}
+	
+	void move_left() {
+		
+	}
+	
+	void set_color() {
+		
+	}
+	
+	void set_type() {
+		
+	}
+	
+}
