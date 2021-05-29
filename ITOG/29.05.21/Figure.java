004ce4cf2e5efa1f5dc4f61417cd41888c81c585
GarlicRoll <75137969+GarlicRoll@users.noreply.github.com>
2021-05-29 19:13:48 +0300

    Add files via upload

---
+++ ITOG/29.05.21/Figure.java
@@ -0,0 +1,221 @@
+//package Tetris;
+
+import java.util.ArrayList;
+import java.util.Random;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Shell;
+
+public class Figure {
+
+	ArrayList<Image> images;
+	Image image;
+	ArrayList<Block> blocks;
+	boolean[][] placement_table; // таблица для расстановки блоков
+	int height;
+	int width;
+	int x; // положение фигуры по верхнему левому блоку
+	int y;
+	Shell shell;
+
+	Figure(int x, int y, ArrayList<Image> images, Shell shell) {
+		this.x = x;
+		this.y = y;
+		this.shell = shell;
+		this.images = images;
+		this.blocks = new ArrayList<>();
+		set_color();
+	}
+	
+	// поворот фигуры
+	public void rotate(boolean[][] matrix_placement_table, int matrix_height, int matrix_width) {
+		boolean[][] placement_table_to_check = MatrixInMatrix.matrixrotate(placement_table); // проверка на возможность поворота
+		if (rotation_check(placement_table_to_check, matrix_placement_table, matrix_height, matrix_width) == true) {
+			placement_table = placement_table_to_check; // изменение таблицы расположения фигуры
+			rotate_blocks(); // изменение внешнего вида
+		}
+	}
+	
+	// проверка на возможность поворота
+	private boolean rotation_check(boolean[][] placement_table_to_check, boolean[][] matrix_placement_table, int matrix_height, int matrix_width) {
+		if (height + x > matrix_width || width + y > matrix_height) { // ширина и высота меняются местами при повороте
+			return false;
+		}
+		else {
+			// если блок удраяется о блок при повороте
+			for (int row = 0; row < width; row++) {
+				for (int column = 0; column < height; column++) {
+					if ((placement_table_to_check[row][column] && matrix_placement_table[row + y][column + x]) == true) {
+						return false;
+					}
+				}
+			}
+		}
+		return true;
+	}
+	
+	// проверка на возможность появления
+	private boolean appearance_check(boolean[][] placement_table_to_check, boolean[][] matrix_placement_table, int matrix_height, int matrix_width) {
+		// если блок удраяется о блок при появлении
+		for (int row = 0; row < height; row++) {
+			for (int column = 0; column < width; column++) {
+				if ((placement_table_to_check[row][column] && matrix_placement_table[row + y][column + x]) == true) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	// сдвиг вниз
+	boolean move_down(boolean[][] matrix_placement_table, int matrix_height, int matrix_width) {
+		// координата + высота - 1 (тк индексация с нуля) + 1 (тк проверка следущего
+		// шага)
+		if (y + height - 1 + 1 > matrix_height - 1) { // если блок достиг границы поля
+			return false;
+		}
+		// если блок удраяется о блок
+		// я проверяю все блоки фигуры, а не только её край, тк фигура может иметь
+		// выступ после края (например, т-фигура)
+		for (int row = 0; row < height; row++) {
+			for (int column = 0; column < width; column++) {
+				if ((placement_table[row][column] && matrix_placement_table[row + y + 1][column + x]) == true) {
+					return false;
+				}
+			}
+		}
+		// если всё хорошо
+		for (Block block : blocks) {
+			block.move_down();
+		}
+		y += 1;
+		return true;
+	}
+
+	// сдвиг вправо
+	boolean move_right(boolean[][] matrix_placement_table, int matrix_height, int matrix_width) {
+		if (x + width - 1 + 1 > matrix_width - 1) { // если блок достиг границы поля
+			return false;
+		}
+		// если блок удраяется о блок
+		for (int row = 0; row < height; row++) {
+			for (int column = 0; column < width; column++) {
+				if ((placement_table[row][column] && matrix_placement_table[row + y][column + x + 1]) == true) {
+					return false;
+				}
+			}
+		}
+		// если всё хорошо
+		for (Block block : blocks) {
+			block.move_right();
+		}
+		x += 1;
+		return true;
+	}
+
+	// сдвиг влево
+	boolean move_left(boolean[][] matrix_placement_table, int matrix_height, int matrix_width) {
+		if (x - 1 < 0) { // если блок достиг границы поля
+			return false;
+		}
+		// если блок удраяется о блок
+		for (int row = 0; row < height; row++) {
+			for (int column = 0; column < width; column++) {
+				if ((placement_table[row][column] && matrix_placement_table[row + y][column + x - 1]) == true) {
+					return false;
+				}
+			}
+		}
+		// если всё хорошо
+		for (Block block : blocks) {
+			block.move_left();
+		}
+		x -= 1;
+		return true;
+	}
+
+	private void set_color() { // случайный цвет
+		Random random = new Random();
+		int color_number = random.nextInt(images.size());
+		this.image = images.get(color_number);
+	}
+
+	public boolean set_type(boolean[][] matrix_placement_table, int matrix_height, int matrix_width) { // случайный вид фигуры
+		Random random = new Random();
+		int type_number = random.nextInt(7);
+		
+		// семь классических фигур
+		if (type_number == 0) {
+			this.placement_table = new boolean[][] { { true, true }, { true, true } };
+		} else if (type_number == 1) {
+			this.placement_table = new boolean[][] { { false, true, false }, { true, true, true } };
+		} else if (type_number == 2) {
+			this.placement_table = new boolean[][] { { true, true, true, true } };
+		} else if (type_number == 3) {
+			this.placement_table = new boolean[][] { { false, true, true }, { true, true, false } };
+		} else if (type_number == 4) {
+			this.placement_table = new boolean[][] { { true, true, false }, { false, true, true } };
+		} else if (type_number == 5) {
+			this.placement_table = new boolean[][] { { false, false, true }, { true, true, true } };
+		} else if (type_number == 6) {
+			this.placement_table = new boolean[][] { { true, false, false }, { true, true, true } };
+		}
+		this.height = placement_table.length;
+		this.width = placement_table[0].length;
+		if (appearance_check(placement_table, matrix_placement_table, matrix_height, matrix_width) == true) { // проверка на возможность появления
+			add_blocks();
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	// добавление блоков
+	private void add_blocks() {
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				if (placement_table[y][x] == true) { // если в таблице размещения true
+					this.blocks.add(new Block(x + this.x, y + this.y, image, shell));
+				}
+			}
+		}
+	}
+
+	private void rotate_blocks() {
+
+		swap_H_W();
+
+		int block_count = 0; // кол-во блоков с изм. координатами
+		for (Block block : blocks) {
+			boolean changed = false;
+			int current_block_count = block_count; // кол-во блоков, которое нужно пропустить
+			for (int y = 0; y < height; y++) {
+				for (int x = 0; x < width; x++) {
+					if (placement_table[y][x] == true) { // если в таблице размещения true
+						if (current_block_count > 0) { // пропуск блока
+							current_block_count--;
+							continue;
+						}
+						block.x = x + this.x;
+						block.y = y + this.y;
+						block.update_base(); // очень незаметная, но важная вещь (особенно когда фигуру поворачивают перед остановкой)
+						changed = true;
+						block_count += 1;
+						break;
+					}
+				}
+				if (changed == true) {
+					break;
+				}
+			}
+		}
+	}
+
+	// смена ширины высотой
+	private void swap_H_W() {
+		int buffer = this.height;
+		this.height = this.width;
+		this.width = buffer;
+	}
+
+}
---
+++ ITOG/29.05.21/Main.java
@@ -0,0 +1,154 @@
+//package Tetris;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.KeyAdapter;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+
+public class Main {
+
+	static int TIME = 300; // мл секунды
+	static int current_time = TIME;
+	static int total_time = 0;
+	static int total_minutes = 0;
+	static int total_seconds = 0;
+
+	static int BLOCK_SIZE = 32;
+
+	static int game_table_height = 21;
+	static int game_table_width = 10;
+
+	static double acceleration_factor = 2;
+
+	static int WINDOW_W = BLOCK_SIZE * game_table_width + BLOCK_SIZE * 5;
+	static int WINDOW_H = BLOCK_SIZE * game_table_height + BLOCK_SIZE * 2;
+
+	static boolean acceleration = false;
+
+	static boolean game_is_on = true;
+
+	public static void main(String[] args) {
+		Display display = new Display();
+		Shell shell = new Shell(display);
+		shell.setSize(WINDOW_W, WINDOW_H);
+
+		///////
+		String source = "src/images/";
+
+		Matrix matrix = new Matrix(game_table_height, game_table_width); // y и x
+
+		Label label_timer = new Label(shell, SWT.BORDER | SWT.CENTER); // время
+		label_timer.setBounds(matrix.width * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE * 4, BLOCK_SIZE * 2);
+		label_timer.setFont(new Font(display, source, 30, 10));
+
+		Label label_score = new Label(shell, SWT.BORDER | SWT.CENTER); // очки
+		label_score.setBounds(matrix.width * BLOCK_SIZE, BLOCK_SIZE * 3, BLOCK_SIZE * 4, BLOCK_SIZE * 2);
+		label_score.setFont(new Font(display, source, 30, 10));
+
+		ArrayList<String> backgrounds_paths = new ArrayList<>(); // фон
+		backgrounds_paths.add(source + "background1.png");
+		backgrounds_paths.add(source + "background2.png");
+		backgrounds_paths.add(source + "background3.png");
+		Random rand = new Random();
+		Image background_image = new Image(display, backgrounds_paths.get(rand.nextInt(backgrounds_paths.size())));
+		Label background_label = new Label(shell, SWT.BORDER | SWT.CENTER);
+		background_label.setImage(background_image);
+		background_label.setBounds(0, 0, game_table_width * BLOCK_SIZE, game_table_height * BLOCK_SIZE);
+
+		ArrayList<String> images_paths = new ArrayList<>(); // добавление путей картинок (цвета блоков)
+		images_paths.add(source + "blue.jpg");
+		images_paths.add(source + "green.jpg");
+		images_paths.add(source + "purple.jpg");
+		images_paths.add(source + "red.jpg");
+		ArrayList<Image> images = make_images(images_paths, display); // преобразование в картинки
+
+		///////
+
+		Runnable timer = new Runnable() { // таймер для игровых вычислений
+			public void run() {
+				if (game_is_on == true) {
+					if (matrix.figures.size() == 0) {
+						Random random = new Random();
+						int x = random.nextInt(matrix.width - 3); // -3 для правой границы, чтобы никто там точно не
+																	// появился
+						if (matrix.add_figure(new Figure(x, 0, images, shell), matrix.placement_table, matrix.height,
+								matrix.width) == false) { // добавление фигуры
+							game_is_on = false;
+						}
+						label_score.setText(String.valueOf(matrix.score));
+						acceleration = false;
+						current_time = (int) (TIME * acceleration_factor);
+
+						if (acceleration_factor > 60) { // ограничение для ускорения
+							acceleration_factor /= 1.1;
+						}
+					}
+					total_time += current_time;
+					total_minutes = total_time / 1000 / 60;
+					total_seconds = total_time / 1000 % 60;
+					label_timer.setText(String.valueOf(total_minutes) + " : " + String.valueOf(total_seconds)); // установка
+																												// текста
+					matrix.figure_shift_loop();
+					display.timerExec(current_time, this);
+				}
+			}
+		};
+
+		display.timerExec(current_time, timer);
+
+		shell.open();
+
+		shell.addKeyListener(new KeyAdapter() { // управление
+			public void keyPressed(KeyEvent e) {
+				if (e.keyCode == SWT.ARROW_DOWN && acceleration == false) {
+					if (matrix.figures.size() != 0) {
+						current_time = 50;
+						acceleration = true;
+					}
+				}
+				// acceleration == true - фигура быстро падает вниз, поворачивать и двигать её
+				// уже нельзя
+				if (e.keyCode == SWT.ARROW_LEFT && acceleration == false) {
+					if (matrix.figures.size() != 0) {
+						matrix.figures.get(0).move_left(matrix.placement_table, matrix.height, matrix.width);
+					}
+				}
+				if (e.keyCode == SWT.ARROW_RIGHT && acceleration == false) {
+					if (matrix.figures.size() != 0) {
+						matrix.figures.get(0).move_right(matrix.placement_table, matrix.height, matrix.width);
+					}
+				}
+				if (e.keyCode == SWT.SPACE && acceleration == false) {
+					if (matrix.figures.size() != 0) {
+						matrix.figures.get(0).rotate(matrix.placement_table, matrix.height, matrix.width);
+					}
+				}
+			}
+		});
+
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+
+		// disposes all associated windows and their components
+		display.dispose();
+
+	}
+
+	private static ArrayList<Image> make_images(ArrayList<String> images_paths, Display display) {
+		ArrayList<Image> images = new ArrayList<>();
+		for (String path : images_paths) {
+			images.add(new Image(display, path));
+		}
+		return images;
+	}
+}
